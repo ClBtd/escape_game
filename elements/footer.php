35a474fdae9d38e7158a605a064b3cb6be0dd87f
Clémentine Bertrand <clementinebertrand@MacBook-Pro-de-Clementine.local>
2023-06-01 22:19:24 +0200

    Fin de la mise en page du header et mise en page du footer

--- elements/footer.php
+++ elements/footer.php
@@ -1,29 +1,29 @@
     
     <footer>
 
+        <ul>
+            <li>
+                <a href="#" title="Instagram" target="_blank"><img src="img/instagram.png" alt="Instagram"></a>
+            </li>
+            <li>
+                <a href="#" title="Facebook" target="_blank"><img src="img/facebook.png" alt="Facebook"></a>
+            </li>
+            <li>
+                <a href="#" title="Twitter" target="_blank"><img src="img/twitter.png" alt="Twitter"></a>
+            </li>
+        </ul>
+
         <div class="contact">
+            <div class="address">
+                <h1 class="title">les dedales du temps</h1>
+                <div class="contact">
+                    <p>30 boulevard des Canuts<br>69004 Lyon</p>
+                    <p>06 19 95 86 16<br>contact@dedalesdutemps.fr</p>
+                </div>
+            </div>
             <img src="img/logo.png" alt="Logo de Dédales du temps">
-            <h1>Les dédales du temps</h1>
-            <address>30 boulevard des Canuts
-                <br>69004 Lyon
-                <br>06 19 95 86 16
-                <br>contact@dedalesdutemps.fr
-            </address>
         </div>
 
-        <div class="social">
-            <ul>
-                <li>
-                    <a href="#" title="Instagram" target="_blank"><img src="img/instagram.png" alt="Instagram"></a>
-                </li>
-                <li>
-                    <a href="#" title="Facebook" target="_blank"><img src="img/facebook.png" alt="Facebook"></a>
-                </li>
-                <li>
-                    <a href="#" title="Twitter" target="_blank"><img src="img/twitter.png" alt="Twitter"></a>
-                </li>
-            </ul>
-        </div>
 
     </footer>
     
