bc7bf4c19ecf06e80cb46cac05376ca13f2136c7
Clémentine Bertrand <clementinebertrand@MacBook-Pro-de-Clementine.local>
2023-06-04 21:14:43 +0200

    Mise en page de l'accueil - partie 1

--- index.php
+++ index.php
@@ -6,17 +6,15 @@ require_once "elements/header.php";
  <main>
 
     <div class="intro">
-        <img src="img/logo.png" alt="Logo des dédales du temps">
         <h1 class="title">les dedales du temps</h1>
-        <h1 class="title">escape game lyon</h3>
-        <h2>Saurez-vous échapper à votre destin et revenir dans le présent ?</h2>
+        <h2>ESCAPE GAME LYON</h2>
+        <h3>Saurez-vous échapper à votre destin pour revenir dans le présent ?</h3>
     </div>
 
     <div class="presentation">
-        <p>Bienvenue dans <strong>Les Dédales du Temps</strong>, une expérience immersive qui vous transporte à travers les méandres du passé ! Préparez-vous à un voyage captivant où chaque seconde compte.</p>
+        <p>Bienvenue dans <strong class="title">les dedales du temps</strong>, une expérience immersive qui vous transporte à travers les méandres du passé ! Préparez-vous à un voyage captivant où chaque seconde compte.</p>
         <p>Votre mission est simple, mais périlleuse : affronter les épreuves du passé qui vous attendent pour revenir sains et saufs dans le présent. Une erreur, un paradoxe non résolu et le futur tel que vous le connaissez pourrait être à jamais modifié. Votre équipe devra faire preuve de logique, d'ingéniosité et de coopération pour résoudre les énigmes, déjouer les pièges.</p>
         <p>Explorez des décors extraordinaires qui vous transporteront selon votre choix dans vers les pharaons égyptiens, auprès des templiers du Moyen Âge, dans les ruelles londoniennes de l'époque victorienne ou au cœur de la seconde guerre mondiale. Chaque salle est un véritable tableau vivant, une fenêtre ouverte sur des époques révolues où l'atmosphère et les détails historiques sont minutieusement reconstitués.</p>
-        <p>Réunissez votre équipe, préparez-vous à franchir les frontières temporelles et lancez-vous dans l'expérience inoubliable des dédales du temps, où les énigmes n'attendent que d'être résolues.</p>
     </div>
 
     <div class="presentation">
